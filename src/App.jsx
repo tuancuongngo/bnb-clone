f93ca2a28a4d2bc840cd97a82ad9bf66512d74df
Cuong Ngo <ngoct@vt.edu>
2024-06-13 19:28:11 -0400

    Add animation on hover for Listing Card

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="container">
       <NavBar />
-      <div className="container ">
+      <div className="container">
         <ListingCard />
       </div>
     </div>
--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import { MapPin, DollarSign, Star, UsersRound } from "lucide-react";
 
 const ListingCard = ({ title, image, location, price, rating, numGuests }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="max-w-sm border rounded-md bg-white text-gray-950 p-3 cursor-pointer">
+    <div
+      className={`group max-w-sm rounded-md bg-white text-gray-950 p-3 cursor-pointer hover:bg-red-500 hover:ring-red-500 duration-300 ${
+        isHovered ? "transform scale-105" : ""
+      }`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <img
         src={image ? image : "/default-card-img.jpg"}
         alt="Main Listing Image"
         className="h-[200px] w-full rounded-md object-cover"
       />
-      <div className="flex-col">
+      <div className="flex-col group-hover:text-white">
         <div className="flex justify-between pt-1 text-xl">
           <div className="font-bold text-pretty">{title}</div>
           <div className="flex items-center gap-0.5">
